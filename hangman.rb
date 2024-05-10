eb6ed0abb2859f55192d84d89d92ff0ebd580d66
Caleb <calebkenyonpdx@gmail.com>
2024-05-09 23:04:17 -0600

    Add save and load method

--- hangman.rb
+++ hangman.rb
@@ -1,6 +1,4 @@
-require "csv"
-
-INCORRCET_GUESS_MAX = 8
+require "json"
 
 # Load viable words into array.
 contents = File.readlines("./google-10000-english-no-swears.txt")
@@ -11,74 +9,122 @@ contents.each do |word|
   end 
 end
 
-def select_game_word(words)
-  game_word = words.sample()
-end 
-
-def create_blanks_of_word(word)
-  blanks = ""
-  word.length.times do 
-    blanks += "_"
+class Hangman
+  INCORRCET_GUESS_MAX = 8
+  def initialize()
+    @guessed_letters = ""
+    @game_word = ""
+    @blanks = ""
+    @incorrect_guesses = 0
   end
-  blanks
-end
 
-def print_blanks(blanks)
-  blanks.each_char do |blank|
-    print(blank + " ")
-  end
-  print("\n")
-end
-
-
-def get_guess(guessed_letters)
-  puts "Enter your single letter guess."
-  while true
-    guess = gets.chomp.downcase
-    if guess.length == 1 && guess.match?(/[[:alpha:]]/) && guessed_letters.include?(guess).!
-      guessed_letters += guess
-      return guess, guessed_letters
-    else
-      puts "Invalid guess--the guess must be a single letter and not a previous letter->#{guessed_letters}."
+  def save_game
+    data = {guessed_letters: @guessed_letters, game_word: @game_word, blanks: @blanks, incorrect_guesses: @incorrect_guesses}
+    json_data = JSON.generate(data)
+    File.open("saves.txt", "w") do |file|
+      file.puts json_data
     end
   end
-end
 
-def check_guess(word, letter, blanks, incorrect_guesses)
-  if word.include?(letter)
-    word.each_char.with_index do |word_letter, index|
-      if word_letter == letter
-        blanks[index] = letter
+  def load_game
+    File.open("saves.txt", "r") do |file|
+      dict = JSON.parse(file.read)
+      @guessed_letters = dict["guessed_letters"]
+      @game_word = dict["game_word"]
+      @blanks = dict["blanks"]
+      @incorrect_guesses = dict["incorrect_guesses"]
+    end
+  end
+
+  def select_game_word(words)
+    @game_word = words.sample()
+  end 
+
+  def create_blanks_of_word()
+    @blanks = ""
+    @game_word.length.times do 
+      @blanks += "_"
+    end
+  end
+
+  def print_blanks()
+    @blanks.each_char do |blank|
+      print(blank + " ")
+    end
+    print("\n")
+  end
+
+
+  def get_input()
+    puts "Enter your single letter guess. Enter 0 to save for later."
+    while true
+      guess = gets.chomp.downcase
+      if guess.length == 1 && guess.match?(/[[:alpha:]]/) && @guessed_letters.include?(guess).!
+        @guessed_letters += guess
+        return guess
+      elsif guess == "0"
+        save_game()
+        return guess
+      else
+        puts "Invalid guess--the guess must be a single letter and not a previous letter->#{@guessed_letters}."
       end
     end
-  else  
-    incorrect_guesses += 1
   end
-  return blanks, incorrect_guesses
-end
 
-def win?(blanks)
-  blanks.include?("_").!
-end
+  def check_guess(letter)
+    if @game_word.include?(letter)
+      @game_word.each_char.with_index do |word_letter, index|
+        if word_letter == letter
+          @blanks[index] = letter
+        end
+      end
+    else  
+      @incorrect_guesses += 1
+    end
+    return 
+  end
 
+  def win?()
+    @blanks.include?("_").!
+  end
 
-def play_game(words)
-  guessed_letters = ""
-  incorrect_guesses = 0
-  current_word = select_game_word(words)
-  blanks = create_blanks_of_word(current_word)
-  guessed_letters = ""
-  while incorrect_guesses <= INCORRCET_GUESS_MAX
-    puts("#{incorrect_guesses} of #{INCORRCET_GUESS_MAX} wrong guesses.")
-    print_blanks(blanks)
-    letter_guess, guessed_letters = get_guess(guessed_letters)
-    blanks, incorrect_guesses = check_guess(current_word, letter_guess, blanks, incorrect_guesses)
-    if win?(blanks)
-      puts("You won with #{incorrect_guesses} wrong guesses! The word was #{current_word}.")
-      return
+  def new_game?()
+    while true
+      puts "Do you want to a new game (n) or to load your last game (l)?"
+      answer = gets.chomp.downcase
+      if answer == "n"
+        return true
+      elsif answer == "l"
+        return false
+      end
     end
   end
-  puts "You lost. The word was #{current_word}"
+
+  def play_game(words)
+    if new_game?()
+      @game_word = select_game_word(words)
+      create_blanks_of_word()
+    else
+      load_game()
+    end
+    while @incorrect_guesses <= INCORRCET_GUESS_MAX
+      puts("#{@incorrect_guesses} of #{INCORRCET_GUESS_MAX} wrong guesses.")
+      print_blanks()
+      letter_guess = get_input()
+      if letter_guess == "0"
+        return
+      end
+      check_guess(letter_guess)
+      if win?()
+        puts("You won with #{@incorrect_guesses} wrong guesses! The word was #{@game_word}.")
+        return
+      end
+    end
+    puts "You lost. The word was #{@game_word}."
+  end
+
 end
 
-play_game(words)
+game = Hangman.new()
+game.play_game(words)
+# game.load_game
